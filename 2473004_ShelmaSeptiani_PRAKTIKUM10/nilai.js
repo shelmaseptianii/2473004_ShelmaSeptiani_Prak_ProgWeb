96ca851f2000b0f8ef294309ed428d121c9cf49e
shelmaseptianii <shelmaseptianii@gmail.com>
2026-05-18 10:13:05 +0700

    add prak 10

---
+++ 2473004_ShelmaSeptiani_PRAKTIKUM10/nilai.js
@@ -0,0 +1,5 @@
+[
+  {"nrp": "1072001", "nama": "Budi", "kelas": "A", "nilai_akhir": 98},
+  {"nrp": "1072002", "nama": "Andi", "kelas": "B", "nilai_akhir": 80},
+  {"nrp": "1072003", "nama": "Cindy", "kelas": "A", "nilai_akhir": 70}
+]
---
+++ 2473004_ShelmaSeptiani_PRAKTIKUM10/nilaiA.js
@@ -0,0 +1,4 @@
+[
+  {"nrp": "1072001", "nama": "Budi", "kelas": "A", "nilai_akhir": 98},
+  {"nrp": "1072003", "nama": "Cindy", "kelas": "A", "nilai_akhir": 70}
+]
